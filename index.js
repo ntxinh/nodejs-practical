742c6ce586d61da711dd22a2956bb0f28c82e9ed
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2017-12-17 22:49:24 +0700

    Add static file serving

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ app.use(bodyParser.json())
 
 routes(app)
 
+// serving static files
+app.use(express.static('public'))
+
 app.get('/', (req, res) =>
     res.send(`Node and express server is running on port ${PORT}`)
 )
